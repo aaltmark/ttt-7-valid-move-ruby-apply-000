28cbd60a6a1ac00dc5e43ae41c71b06439c6ee83
Adam Altmark <adamaltmark@gmail.com>
2020-04-20 21:40:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,9 @@
 def valid_move?(board, index)
-  position_taken?(board, index) == false && index.between?(0,8)
+  if position_taken?(board, index) == false && index.between?(0,8)
+    true
+  else
+    false
+  end
 end
 
 
